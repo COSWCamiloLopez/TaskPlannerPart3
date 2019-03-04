32e247ab2356353902d297c3fc5db65c7bbdf2a5
Camilo Lopez <camilino123@gmail.com>
2019-03-03 22:09:13 -0500

    Firebase deploy done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAFPNr5dBlaiLb1QJqp1YOTNRIGzn0QLpQ",
+    authDomain: "task-planner-app-f396d.firebaseapp.com",
+    databaseURL: "https://task-planner-app-f396d.firebaseio.com",
+    projectId: "task-planner-app-f396d",
+    storageBucket: "task-planner-app-f396d.appspot.com",
+    messagingSenderId: "495126587051"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
